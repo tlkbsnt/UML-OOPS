6b90aa97cea077b6bb93b445e0f3e1b413c67d92
tlkbsnt <109103556+tlkbsnt@users.noreply.github.com>
2022-08-22 12:16:02 +0200

    Update QuizController.java

--- QuizController.java
+++ QuizController.java
@@ -1,16 +1,11 @@
 package fr.epita.quiz;
 
-import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.CheckBox;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.*;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -18,6 +13,9 @@ import java.net.URL;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Random;
 import java.util.ResourceBundle;
 public class QuizController implements Initializable {
     public TextArea questionTextArea;
@@ -26,57 +24,99 @@ public class QuizController implements Initializable {
     public CheckBox thirdCheckBox;
     public CheckBox forthCheckBox;
     public TableView<Score> quizTableView;
-    public TableColumn<Score, String>  nameColumn;
-    public TableColumn<Score, String>  scoreColumn;
-    public String studentName;
-    public String studentEmail;
-    public int quizScore;
-    public ObservableList<Score> scoreList;
+    public TableColumn<Score, String> nameColumn;
+    public TableColumn<Score, String> scoreColumn;
+    public Label labelQuestionNumber;
+    public Button nextQuestionBtn;
+
+    Connection connection = DBConnection.getConnector();
+    Statement statement;
+    ResultSet resultSet;
+    public ObservableList<Quiz> quizObservableList;
+    int x = 0;
+    ArrayList<String> questionNumberList = new ArrayList<>();
+    ArrayList<String> questionList = new ArrayList<>();
+    ArrayList<String> answerList = new ArrayList<>();
+    ArrayList<String> answerOption1list = new ArrayList<>();
+    ArrayList<String> answerOption2list = new ArrayList<>();
+    ArrayList<String> answerOption3list = new ArrayList<>();
+    public void  fetchQuestion() {
+        try {
+            statement = connection.createStatement();
+            resultSet = statement.executeQuery("Select * from question");
+
+            while (resultSet.next()) {
+                //resultSet.getString("sn")+ " " +
+                String questionNumber = resultSet.getString("qn");
+                String question = resultSet.getString("question");
+                String answer = resultSet.getString("answer");
+                String answerOption1 = resultSet.getString("answerOption1");
+                String answerOption2 = resultSet.getString("answerOption2");
+                String answerOption3 = resultSet.getString("answerOption3");
+                // Below values are add in the Arraylist with corresponding above defined ArrayList
+                questionNumberList.add(String.valueOf(questionNumber));
+                questionList.add(question);
+                answerList.add(answer);
+                answerOption1list.add(answerOption1);
+                answerOption2list.add(answerOption2);
+                answerOption3list.add(answerOption3);
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        // From here value of the CheckBox generate  randomly which set random value in Checkbox
+        ArrayList<String> setCheckboxRandom = new ArrayList<>();
+        setCheckboxRandom.add(answerList.get(x));
+        setCheckboxRandom.add(answerOption1list.get(x));
+        setCheckboxRandom.add(answerOption2list.get(x));
+        setCheckboxRandom.add(answerOption3list.get(x));
+
+        int []indic = new int[setCheckboxRandom.size()];
+        Random rnd = new Random();
+
+        for(int i=0; i<indic.length; i++){
+            int j = rnd.nextInt(i+1);
+            indic[i] = indic[j];
+            indic[j] = i;
+        }
+        ArrayList listSet = new ArrayList<>();
+        for (int i : indic) {
+            listSet.add(setCheckboxRandom.get(i));
+        }
+        // From here End the set of the Random value in CheckBox
+                    labelQuestionNumber.setText("Qn : " + questionNumberList.get(x) + " ");
+                    questionTextArea.setText(questionList.get(x));
+                    firstCheckBox.setText(listSet.get(0).toString());
+                    secondCheckBox.setText(listSet.get(1).toString());
+                    thirdCheckBox.setText(listSet.get(2).toString());
+                    forthCheckBox.setText(listSet.get(3).toString());
+        ++x;
+
+    }
     public void onSubmitBtn() {
+
     }
     public void onNextQuestionBtn() {
+
 
     }
     public void onQuitBtn() {
         System.exit(0);
     }
-
     public void onLoginAdmin() throws IOException {
-
-
-
-
         Stage stageLogin = new Stage();
         Parent root = FXMLLoader.load(getClass().getResource("AdminController.fxml"));
         Scene scene = new Scene(root);
         stageLogin.setScene(scene);
         stageLogin.show();
-
         Stage stage = (Stage)this.questionTextArea.getScene().getWindow();
         stage.close();
     }
-    public void loadScore(){
-
-            Score score = new Score(null,quizScore);
-            String query = "select  Student.name, Score.scores from Score left join Student on Score.Sn = Student.Sn";
-            try {
-                Connection conn = DBConnection.getConnector();
-                this.scoreList = FXCollections.observableArrayList();
-                ResultSet resultSet = conn.createStatement().executeQuery(query);
-                while (resultSet.next()) {
-                    this.scoreList.add(new Score(resultSet.getString(1), resultSet.getInt(2)));
-                    this.scoreColumn.setCellValueFactory(new PropertyValueFactory<Score, String>("name"));
-                    this.nameColumn.setCellValueFactory(new PropertyValueFactory<Score, String>("scores"));
-                    this.quizTableView.setItems(null);
-                    this.quizTableView.setItems(this.scoreList);
-                }
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-        }
+    public void loadScore() {
+    }
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        loadScore();
 
     }
 }
